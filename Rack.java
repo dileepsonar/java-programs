c3892766353b766f39c0094e317aefbdc43452ba
dileepsonar <112550671+dileepsonar@users.noreply.github.com>
2022-09-09 08:05:52 +0530

    mehods

---
+++ Rack.java
@@ -0,0 +1,37 @@
+class Rack {
+
+String brandName;
+
+short price;
+ char color;
+
+String types;
+
+boolean isitmadebywood;
+
+Rack (String brandName, short price, char color,String types, boolean isitmadebywood){
+
+this.brandName =brandName;
+
+this.price =price;
+
+this.color = color;
+
+this.types= types;
+
+this.isitmadebywood= isitmadebywood;
+}
+
+static void noordrawer(){
+
+int a =465;
+
+int b=890;
+int c= 900;
+int sum =a+b+c;
+System.out.println (sum);
+}
+public static void main(String[]args){
+
+}
+}
